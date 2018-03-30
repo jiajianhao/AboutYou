506b0e03dd605acf90d91cc315fa864ef73d444e
贾建昊 <980497452@qq.com>
2018-03-30 13:12:53 +0800

    销毁处理

--- AboutYou/NSObject+Category.m
+++ AboutYou/NSObject+Category.m
@@ -24,6 +24,8 @@
 #pragma mark KVO
 
 -(void)bindKey:(NSString *)key WithBlock:(objectKVOBlock)block{
+    NSAssert(block && key, @"参数不能为空");
+
     if (key.length==0) {
         return ;
     }
@@ -37,13 +39,13 @@
 //添加监听
 -(void)addObserverWithKey:(NSString*)key{
     
- 
     [self addObserver:self forKeyPath:key options:NSKeyValueObservingOptionNew|NSKeyValueObservingOptionOld context:(__bridge void *_Nullable)self.bindId];
-}
+ }
 //实时监听
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context{
     self.bindId = (__bridge NSString *)(context);
     [self handleKeypath:keyPath AndChange:change];
+    
 }
 //监听处理
 -(void)handleKeypath:(NSString*)keypath AndChange:(NSDictionary*)change{
@@ -55,30 +57,48 @@
 }
 //生成唯一标识符
 -(NSString*)productUniqueId:(NSString*)key{
-    return [NSString stringWithFormat:@"jjh_%@",key];
+    //根据"对象内存地址"和"key值"生成唯一标识符
+    return [NSString stringWithFormat:@"%p....%@",self,key];
 }
 
 //Setter && Getter
 -(void)setBindId:(NSString *)bindId{
     objc_setAssociatedObject(self, @selector(bindId), bindId, OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
+
 -(void)setBlockDic:(NSMutableDictionary *)blockDic{
     objc_setAssociatedObject(self, @selector(blockDic), blockDic, OBJC_ASSOCIATION_COPY_NONATOMIC);
 }
-
+ 
 -(NSString *)bindId{
     return  objc_getAssociatedObject(self, _cmd);
 }
 -(NSMutableDictionary *)blockDic{
     if (!objc_getAssociatedObject(self, _cmd))
     {
-         objc_setAssociatedObject(self,_cmd,@{}.mutableCopy,OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    }
+        objc_setAssociatedObject(self, _cmd, @{}.mutableCopy, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+     }
     return objc_getAssociatedObject(self, _cmd);
 }
 
 #pragma mark - 销毁处理
 -(void)dealloc{
+    [self removeAllObserver];
+}
+-(void)removeAllObserver{
+    // 如果没有初始化好，那么就不往下执行，防止引起崩溃。
+//    if (![UIApplication sharedApplication].delegate) {
+//        return;
+//    }
+    if (!self.blockDic.allKeys.count) {
+        return;
+    }
     
+     for (NSString *keyStr in self.blockDic.allKeys) {
+         NSString *key = [keyStr componentsSeparatedByString:@"...."].lastObject;
+         [self removeObserver:self forKeyPath:key];
+
+    }
+    [self.blockDic removeAllObjects];
 }
 @end
